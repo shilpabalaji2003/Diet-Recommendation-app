109c2af40bb9715c53063f24f9e1e485e2560e8c
shilpabalaji2003 <115817942+shilpabalaji2003@users.noreply.github.com>
2024-03-20 20:13:50 +0530

    news added

---
+++ Mini project source code/Streamlit Frontend/pages/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import feedparser
+
+app = Flask(__name__)
+
+def fetch_news(feed_url, num_articles):
+    feed = feedparser.parse(feed_url)
+    articles = []
+    for entry in feed.entries[:num_articles]:
+        articles.append({'title': entry.title, 'link': entry.link})
+    return articles
+
+@app.route('/latest_news')
+def latest_news():
+    # URL of the BBC News RSS feed
+    NEWS_FEED_URL = "http://feeds.bbci.co.uk/news/world/asia/india/rss.xml"
+    
+    # Number of articles to display
+    NUM_ARTICLES = 5
+    
+    articles = fetch_news(NEWS_FEED_URL, NUM_ARTICLES)
+    
+    return render_template('latest_news.html', articles=articles)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
